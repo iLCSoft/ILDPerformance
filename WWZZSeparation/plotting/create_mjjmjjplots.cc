95ba9a516469e028ae52e3d2f1c3a84332ca0929
Jakob Beyer <beyerjac@flc36.desy.de>
2018-10-01 10:02:10 +0200

    Fixing indentation, avoid memory leak
    Switched indentation in all C++ files and some others to spaces. Added
    deletes to avoid memory leak in ROOT macro.

--- WWZZSeparation/plotting/create_mjjmjjplots.cc
+++ WWZZSeparation/plotting/create_mjjmjjplots.cc
@@ -11,114 +11,114 @@ float string_to_float( string str ) {
 }
 
 void create_mjjmjjplots(
-		string luminosity_str, string e_beam_pol_str, string p_beam_pol_str,
-		string input_dir_base ) {
+    string luminosity_str, string e_beam_pol_str, string p_beam_pol_str,
+    string input_dir_base ) {
 
-	// convert input to string
-	float luminosity = string_to_float(luminosity_str);
-	float e_beam_pol = string_to_float(e_beam_pol_str)/100; // -> from %
-	float p_beam_pol = string_to_float(p_beam_pol_str)/100;
+  // convert input to string
+  float luminosity = string_to_float(luminosity_str);
+  float e_beam_pol = string_to_float(e_beam_pol_str)/100; // -> from %
+  float p_beam_pol = string_to_float(p_beam_pol_str)/100;
 
-	// Physical constants (assumed to not change all that much)
-	float M_Z = 91.19; 
-	float M_W = 80.38;
+  // Physical constants (assumed to not change all that much)
+  float M_Z = 91.19; 
+  float M_W = 80.38;
 
-	// Standard ROOT plot setting
-	gROOT->Reset();
-	gStyle->SetOptStat(0);
-	TH2::SetDefaultSumw2();
+  // Standard ROOT plot setting
+  gROOT->Reset();
+  gStyle->SetOptStat(0);
+  TH2::SetDefaultSumw2();
 
-	// Names of input files to be included
-	vector<string> filenames;
-	filenames.push_back(( input_dir_base + "/vvxyyx/vvxyyx.root" ).c_str());
-	filenames.push_back(( input_dir_base + "/vvxxxx/vvxxxx.root" ).c_str());
-	filenames.push_back(( input_dir_base + "/vvyyyy/vvyyyy.root" ).c_str());
+  // Names of input files to be included
+  vector<string> filenames;
+  filenames.push_back(( input_dir_base + "/vvxyyx/vvxyyx.root" ).c_str());
+  filenames.push_back(( input_dir_base + "/vvxxxx/vvxxxx.root" ).c_str());
+  filenames.push_back(( input_dir_base + "/vvyyyy/vvyyyy.root" ).c_str());
 
-	// Name of TTree in input files
-	string input_tree_name = "eventinfo";
+  // Name of TTree in input files
+  string input_tree_name = "eventinfo";
 
-	// Histograms for WW/ZZ events
-	TH1* h1_WW = new TH1D("m_WW",
-				"WW/ZZ separation, averaged dijet-mass; (m_{jj,1} + m_{jj,2})/2 [GeV]; Events",
-				70, 50, 120);
-	TH1* h1_ZZ = new TH1D("m_ZZ",
-				"WW/ZZ separation, averaged dijet-mass; (m_{jj,1} + m_{jj,2})/2 [GeV]; Events",
-				70, 50, 120);
-	TH2* h2_WW = new TH2D("m_m_WW",
-					"WW/ZZ separation, 2D of dijet-masses; m_{jj,1} [GeV];  m_{jj,2} [GeV]",
-					35, 50, 120, 35, 50, 120);
-	TH2* h2_ZZ = new TH2D("m_m_ZZ",
-					"WW/ZZ separation, 2D of dijet-masses; m_{jj,1} [GeV];  m_{jj,2} [GeV]",
-					35, 50, 120, 35, 50, 120);
-
-
-	// Fill histograms for each input file
-	for (int i = 0; i<filenames.size(); i++) {
-		// Read in input TTree
-		TFile *file = TFile::Open((filenames[i]).c_str());
-		TTree *tree = (TTree*)file->Get(input_tree_name.c_str());
-
-		int nEntries = tree->GetEntries();
-
-		// Read sample specific variables, should be same for all files
-		float cross_section;
-		int sample_e_pol, sample_p_pol;
-		tree->SetBranchAddress("cross_section", &cross_section);
-		tree->SetBranchAddress("e_pol", &sample_e_pol);
-		tree->SetBranchAddress("p_pol", &sample_p_pol);
-		tree->GetEntry(0);
-
-		// Calculate weight for this input sample
-		float polarisation_weight =
-				( 1 + sample_e_pol * e_beam_pol )/2 *
-				( 1 + sample_p_pol * p_beam_pol )/2;
-		float weight = ( cross_section * luminosity / nEntries ) * polarisation_weight;
+  // Histograms for WW/ZZ events
+  TH1* h1_WW = new TH1D("m_WW",
+                        "WW/ZZ separation, averaged dijet-mass; (m_{jj,1} + m_{jj,2})/2 [GeV]; Events",
+                        70, 50, 120);
+  TH1* h1_ZZ = new TH1D("m_ZZ",
+                        "WW/ZZ separation, averaged dijet-mass; (m_{jj,1} + m_{jj,2})/2 [GeV]; Events",
+                        70, 50, 120);
+  TH2* h2_WW = new TH2D("m_m_WW",
+                        "WW/ZZ separation, 2D of dijet-masses; m_{jj,1} [GeV];  m_{jj,2} [GeV]",
+                        35, 50, 120, 35, 50, 120);
+  TH2* h2_ZZ = new TH2D("m_m_ZZ",
+                        "WW/ZZ separation, 2D of dijet-masses; m_{jj,1} [GeV];  m_{jj,2} [GeV]",
+                        35, 50, 120, 35, 50, 120);
 
 
-		// Declare variables that are to be connected to TBranches of input file
-		int true_event_type;
-		float pair1_mass, pair2_mass;
-		int passed_preselection;
+  // Fill histograms for each input file
+  for (int i = 0; i<filenames.size(); i++) {
+    // Read in input TTree
+    TFile *file = TFile::Open((filenames[i]).c_str());
+    TTree *tree = (TTree*)file->Get(input_tree_name.c_str());
 
-		// Connect TBranch to addresses of the declared variables
-		tree->SetBranchAddress("true_event_type", &true_event_type);
-		tree->SetBranchAddress("pair1_mass", &pair1_mass);
-		tree->SetBranchAddress("pair2_mass", &pair2_mass);
-		tree->SetBranchAddress("passed_preselection", &passed_preselection);
+    int nEntries = tree->GetEntries();
 
-		// Fill histograms with each entry
-		for (int j = 0; j<nEntries; j++) {
-			tree->GetEntry(j);
-			if ( passed_preselection != 1 ) { continue; }
-			if ( true_event_type == 1 ) {
-				h1_WW->Fill((pair1_mass+pair2_mass)/2., weight);
-				h2_WW->Fill(pair1_mass, pair2_mass, weight);
-			}
-			else if ( true_event_type == 2 ) {
-				h1_ZZ->Fill((pair1_mass+pair2_mass)/2., weight);
-				h2_ZZ->Fill(pair1_mass, pair2_mass, weight);
-			}
-		}
+    // Read sample specific variables, should be same for all files
+    float cross_section;
+    int sample_e_pol, sample_p_pol;
+    tree->SetBranchAddress("cross_section", &cross_section);
+    tree->SetBranchAddress("e_pol", &sample_e_pol);
+    tree->SetBranchAddress("p_pol", &sample_p_pol);
+    tree->GetEntry(0);
 
-	}
+    // Calculate weight for this input sample
+    float polarisation_weight =
+                          ( 1 + sample_e_pol * e_beam_pol )/2 *
+                          ( 1 + sample_p_pol * p_beam_pol )/2;
+    float weight = ( cross_section * luminosity / nEntries ) * polarisation_weight;
 
-	// Normalization the 2D mjj:mjj distributions so that ZZ and WW are comparable
-	h2_WW->Scale(1./ h2_WW->Integral());
-	h2_ZZ->Scale(1./ h2_ZZ->Integral());
 
-	// WW = blue, ZZ = red
-	h1_WW->SetLineColor(4);
-	h1_ZZ->SetLineColor(2);
-	h2_WW->SetLineColor(4);
-	h2_ZZ->SetLineColor(2);
+    // Declare variables that are to be connected to TBranches of input file
+    int true_event_type;
+    float pair1_mass, pair2_mass;
+    int passed_preselection;
 
-	// Do drawing and saving of plots
-	TCanvas *canvas_h1 = new TCanvas("can_h1", "", 0, 0, 600, 600);
-	h1_WW->Draw("hist");
-	h1_ZZ->Draw("hist same");
-	
-	gPad->Update();
-	float h1_y_min = gPad->GetFrame()->GetY1();
+    // Connect TBranch to addresses of the declared variables
+    tree->SetBranchAddress("true_event_type", &true_event_type);
+    tree->SetBranchAddress("pair1_mass", &pair1_mass);
+    tree->SetBranchAddress("pair2_mass", &pair2_mass);
+    tree->SetBranchAddress("passed_preselection", &passed_preselection);
+
+    // Fill histograms with each entry
+      for (int j = 0; j<nEntries; j++) {
+      tree->GetEntry(j);
+      if ( passed_preselection != 1 ) { continue; }
+      if ( true_event_type == 1 ) {
+        h1_WW->Fill((pair1_mass+pair2_mass)/2., weight);
+        h2_WW->Fill(pair1_mass, pair2_mass, weight);
+      }
+      else if ( true_event_type == 2 ) {
+        h1_ZZ->Fill((pair1_mass+pair2_mass)/2., weight);
+        h2_ZZ->Fill(pair1_mass, pair2_mass, weight);
+      }
+    }
+
+  }
+
+  // Normalization the 2D mjj:mjj distributions so that ZZ and WW are comparable
+  h2_WW->Scale(1./ h2_WW->Integral());
+  h2_ZZ->Scale(1./ h2_ZZ->Integral());
+
+  // WW = blue, ZZ = red
+  h1_WW->SetLineColor(4);
+  h1_ZZ->SetLineColor(2);
+  h2_WW->SetLineColor(4);
+  h2_ZZ->SetLineColor(2);
+
+  // Do drawing and saving of plots
+  TCanvas *canvas_h1 = new TCanvas("can_h1", "", 0, 0, 600, 600);
+  h1_WW->Draw("hist");
+  h1_ZZ->Draw("hist same");
+
+  gPad->Update();
+  float h1_y_min = gPad->GetFrame()->GetY1();
   float h1_y_max = gPad->GetFrame()->GetY2();
 
   TLine* h1_W_line = new TLine(M_W, h1_y_min, M_W, h1_y_max);
@@ -128,41 +128,47 @@ void create_mjjmjjplots(
   h1_W_line->Draw("same");
   h1_Z_line->Draw("same");
 
-	string plot_name_h1 = input_dir_base + "/m.pdf";
-	canvas_h1->Print(plot_name_h1.c_str());
+  string plot_name_h1 = input_dir_base + "/m.pdf";
+  canvas_h1->Print(plot_name_h1.c_str());
 
-	TCanvas *canvas_h2 = new TCanvas("can_h2", "", 0, 0, 800, 800);
-	h2_WW->Draw("box");
-	h2_ZZ->Draw("box same");
-	
-	gPad->Update();
+  TCanvas *canvas_h2 = new TCanvas("can_h2", "", 0, 0, 800, 800);
+  h2_WW->Draw("box");
+  h2_ZZ->Draw("box same");
+
+  gPad->Update();
   float h2_x_min = gPad->GetFrame()->GetX1();
   float h2_x_max = gPad->GetFrame()->GetX2();
   float h2_y_min = gPad->GetFrame()->GetY1();
   float h2_y_max = gPad->GetFrame()->GetY2();
 
-	TLine* h2_W_xline = new TLine(h2_x_min, M_W, h2_x_max, M_W);
-	TLine* h2_Z_xline = new TLine(h2_x_min, M_Z, h2_x_max, M_Z);
-	TLine* h2_W_yline = new TLine(M_W, h2_y_min, M_W, h2_y_max);
-	TLine* h2_Z_yline = new TLine(M_Z, h2_y_min, M_Z, h2_y_max);
-	h2_W_xline->SetLineColorAlpha(9, 0.5);
-	h2_W_yline->SetLineColorAlpha(9, 0.5);
-	h2_Z_xline->SetLineColorAlpha(46, 0.5);
-	h2_Z_yline->SetLineColorAlpha(46, 0.5);
-	h2_W_xline->Draw("same");
-	h2_W_yline->Draw("same");
-	h2_Z_xline->Draw("same");
-	h2_Z_yline->Draw("same");
+  TLine* h2_W_xline = new TLine(h2_x_min, M_W, h2_x_max, M_W);
+  TLine* h2_Z_xline = new TLine(h2_x_min, M_Z, h2_x_max, M_Z);
+  TLine* h2_W_yline = new TLine(M_W, h2_y_min, M_W, h2_y_max);
+  TLine* h2_Z_yline = new TLine(M_Z, h2_y_min, M_Z, h2_y_max);
+  h2_W_xline->SetLineColorAlpha(9, 0.5);
+  h2_W_yline->SetLineColorAlpha(9, 0.5);
+  h2_Z_xline->SetLineColorAlpha(46, 0.5);
+  h2_Z_yline->SetLineColorAlpha(46, 0.5);
+  h2_W_xline->Draw("same");
+  h2_W_yline->Draw("same");
+  h2_Z_xline->Draw("same");
+  h2_Z_yline->Draw("same");
 
-	string plot_name_h2 = input_dir_base + "/m_m.pdf";
-	canvas_h2->Print(plot_name_h2.c_str());
+  string plot_name_h2 = input_dir_base + "/m_m.pdf";
+  canvas_h2->Print(plot_name_h2.c_str());
 
-	// Avoid memory leaks
-	delete h1_WW;
-	delete h2_WW;
-	delete h1_ZZ;
-	delete h2_ZZ;
-	delete canvas_h1;
-	delete canvas_h2;
+  // Avoid memory leaks
+  delete h1_WW;
+  delete h2_WW;
+  delete h1_ZZ;
+  delete h2_ZZ;
+  delete h1_W_line;
+  delete h1_Z_line;
+  delete h2_W_xline;
+  delete h2_Z_xline;
+  delete h2_W_yline;
+  delete h2_Z_yline;
+  delete canvas_h1;
+  delete canvas_h2;
 
 }
--- WWZZSeparation/processor/src/WWZZProcessor.cc
+++ WWZZSeparation/processor/src/WWZZProcessor.cc
@@ -4,54 +4,54 @@ WWZZProcessor aWWZZProcessor ;
 
 WWZZProcessor::WWZZProcessor() : Processor("WWZZProcessor") {
 
-    _description = "WWZZProcessor: Search for e+e- -> vvWW/vvZZ -> vvqqqq at the ILD detector" ;
+  _description = "WWZZProcessor: Search for e+e- -> vvWW/vvZZ -> vvqqqq at the ILD detector" ;
 
 
-    // Standard input collections
-    registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
-            "InputAllPFOsCollection",  
-            "Name of the PFOs collection", 
-            _colAllPFOs,   
-            std::string("PandoraPFOs") 
-    );      
+  // Standard input collections
+  registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+      "InputAllPFOsCollection",  
+      "Name of the PFOs collection", 
+      _colAllPFOs,   
+      std::string("PandoraPFOs") 
+  );      
 
-    registerInputCollection( LCIO::MCPARTICLE,
-            "MCParticleCollection",
-            "Name of the MC particle collection",
-            _colMC,
-            std::string("MCParticle") 
-    );
-
-
-	// Process specific input collections
-    registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
-            "InputFastJetsCollection",  
-            "Name of the jet collection", 
-            _colFastJets,   
-            std::string("FastJets") 
-    );      
-
-	registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
-            "InputIsolepsCollection",
-            "Name of the isolated lepton collection",
-            _colIsoleps,
-            std::string("Isoleps") 
-    );
+  registerInputCollection( LCIO::MCPARTICLE,
+      "MCParticleCollection",
+      "Name of the MC particle collection",
+      _colMC,
+      std::string("MCParticle") 
+  );
 
 
-	// Name of output root file
-    registerProcessorParameter( "RootFileName",
-            "Name of Root file (default: output.root)",
-            _rootfilename,
-            std::string("/nfs/dust/ilc/group/ild/beyerjac/VBS/nunu_hadrons/output/root_files/output.root") 
-    );
+  // Process specific input collections
+  registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+      "InputFastJetsCollection",  
+      "Name of the jet collection", 
+      _colFastJets,   
+      std::string("FastJets") 
+  );      
 
-	// Cross section of sample
-	registerProcessorParameter( "CrossSection",
-			"Cross section of sample stdhep file",
-			_cross_section,
-			float(0.0)
-	);
+  registerInputCollection( LCIO::RECONSTRUCTEDPARTICLE,
+      "InputIsolepsCollection",
+      "Name of the isolated lepton collection",
+      _colIsoleps,
+      std::string("Isoleps") 
+  );
+
+
+  // Name of output root file
+  registerProcessorParameter( "RootFileName",
+      "Name of Root file (default: output.root)",
+      _rootfilename,
+      std::string("/nfs/dust/ilc/group/ild/beyerjac/VBS/nunu_hadrons/output/root_files/output.root") 
+  );
+
+  // Cross section of sample
+  registerProcessorParameter( "CrossSection",
+      "Cross section of sample stdhep file",
+      _cross_section,
+      float(0.0)
+  );
 			
 
 }
@@ -62,28 +62,28 @@ void WWZZProcessor::init() {
   streamlog_out(DEBUG) << "Initializing." << std::endl ;
 
   _otfile = new TFile( _rootfilename.c_str() , "RECREATE" );   
-	_tree = new TTree( "eventinfo" , "events" );
-  
+  _tree = new TTree( "eventinfo" , "events" );
+
   streamlog_out(DEBUG) << "Created file and tree." << std::endl ;
 
-	// Create tree branches and link them to the variables in the info classes
-	//
-	_tree->Branch("N_evt",	&_info.evtN,	"N_evt/I");	
+  // Create tree branches and link them to the variables in the info classes
+  //
+  _tree->Branch("N_evt",	&_info.evtN,	"N_evt/I");	
 
-	_tree->Branch("cross_section",	&_info.process.cross_section,	"cross_section/F");
-	_tree->Branch("e_pol",			&_info.process.e_pol,			"e_pol/I");
-	_tree->Branch("p_pol",			&_info.process.p_pol,			"p_pol/I");
+  _tree->Branch("cross_section",	&_info.process.cross_section,	"cross_section/F");
+  _tree->Branch("e_pol",			&_info.process.e_pol,			"e_pol/I");
+  _tree->Branch("p_pol",			&_info.process.p_pol,			"p_pol/I");
 
-	_tree->Branch("true_event_type",		&_info.true_event_type,		"true_event_type/I");
-	_tree->Branch("passed_preselection",	&_info.passed_preselection,	"passed_preselection/I");
+  _tree->Branch("true_event_type",		&_info.true_event_type,		"true_event_type/I");
+  _tree->Branch("passed_preselection",	&_info.passed_preselection,	"passed_preselection/I");
 
-	_tree->Branch("pair1_mass",	&_info.observ.pair1_mass,	"pair1_mass/F");	
-	_tree->Branch("pair2_mass",	&_info.observ.pair2_mass,	"pair2_mass/F");	
-	
+  _tree->Branch("pair1_mass",	&_info.observ.pair1_mass,	"pair1_mass/F");	
+  _tree->Branch("pair2_mass",	&_info.observ.pair2_mass,	"pair2_mass/F");	
+
   streamlog_out(DEBUG) << "Created tree branches." << std::endl ;
 
-    _nRun = 0 ;
-    _nEvt = 0 ;
+  _nRun = 0 ;
+  _nEvt = 0 ;
 }
 
 
@@ -101,65 +101,65 @@ void WWZZProcessor::processEvent( LCEvent * evt ) {
   streamlog_out(DEBUG) << "Starting to process event " << evt->getEventNumber() 
      << " in run " << evt->getRunNumber() << std::endl;
 
-	// Reset event variables to default, will be set within processor
-	_info.init();
+  // Reset event variables to default, will be set within processor
+  _info.init();
 
-	_info.evtN = _nEvt;
+  _info.evtN = _nEvt;
 
-	// (Get information from the event header (e.g. cross-section))
-	// -> Currently unavailable from stdhep file -> Use manual input
-	_info.process.cross_section = _cross_section; // evt->getParameters().getFloatVal("CrossSection_fb");
-	_info.process.e_pol = -1; //evt->getParameters().getFloatVal("Pol_em");
-	_info.process.p_pol = +1; //evt->getParameters().getFloatVal("Pol_ep");
-  
+  // (Get information from the event header (e.g. cross-section))
+  // -> Currently unavailable from stdhep file -> Use manual input
+  _info.process.cross_section = _cross_section; // evt->getParameters().getFloatVal("CrossSection_fb");
+  _info.process.e_pol = -1; //evt->getParameters().getFloatVal("Pol_em");
+  _info.process.p_pol = +1; //evt->getParameters().getFloatVal("Pol_ep");
+
   streamlog_out(DEBUG) << "Start reading input collections." << std::endl;
 
 	// All of these collections should always be non-empty
   LCCollection* colAllPFOs   = evt->getCollection( _colAllPFOs ) ;
-	LCCollection* colFastJets  = evt->getCollection( _colFastJets ) ;
+  LCCollection* colFastJets  = evt->getCollection( _colFastJets ) ;
   LCCollection* colMC 			 = evt->getCollection( _colMC ) ;
 
-	// Isoleps could be empty (ideally)
-	LCCollection* colIsoleps = NULL;
-    try{
-        colIsoleps = evt->getCollection( _colIsoleps );
-    }
-    catch( lcio::DataNotAvailableException e )
-    {
-        streamlog_out(WARNING) << _colIsoleps << " collection not available" << std::endl;
-        colIsoleps = NULL;
-    }
+  // Isoleps could be empty (ideally)
+  LCCollection* colIsoleps = NULL;
+  try{
+    colIsoleps = evt->getCollection( _colIsoleps );
+  }
+  catch( lcio::DataNotAvailableException e )
+  {
+    streamlog_out(WARNING) << _colIsoleps << " collection not available" << std::endl;
+    colIsoleps = NULL;
+  }
 
-	// Call analysis script on event
-    streamlog_out(DEBUG) << "Analysing event." << std::endl ;
+  // Call analysis script on event
+  streamlog_out(DEBUG) << "Analysing event." << std::endl ;
 
-	analyseEvent( colMC, colAllPFOs, colFastJets, colIsoleps, _info );
+  analyseEvent( colMC, colAllPFOs, colFastJets, colIsoleps, _info );
 
   streamlog_out(DEBUG) << "Running event selection." << std::endl;
 
-	eventSelection( _info );		
-  
+  eventSelection( _info );		
+
   streamlog_out(DEBUG) << "Writing to tree." << std::endl;
 
-	// Fill the event into the tree
-	_tree->Fill();
-  
+  // Fill the event into the tree
+  _tree->Fill();
+
   streamlog_out(DEBUG) << "Done with event." << std::endl;
 
-    _nEvt ++ ;
+  _nEvt ++ ;
 }
 
 
 void WWZZProcessor::check( LCEvent * /*evt*/ ) { 
-    // nothing to check here - could be used to fill checkplots in reconstruction processor
+  // nothing to check here - could be used to fill checkplots in reconstruction processor
 }
 
 
 void WWZZProcessor::end(){ 
 
-    _otfile->cd();
-    _tree->Write();
-    _otfile->Close();
-    delete _otfile;
+  _otfile->cd();
+  _tree->Write();
+  _otfile->Close();
+  delete _otfile;
 }
 
